fb4566d64104202520f45f95dad17ff75010aaeb
Guilherme Ventura de Souza <venturalp@gmail.com>
2021-04-21 21:29:44 -0300

    test(signup): return also an error with a message when the param email is not received

--- src/presentation/controllers/__tests__/signup.spec.ts
+++ src/presentation/controllers/__tests__/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('Should return 400 if no email is provided', () => {
+    // sut = System Under Test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Foo Bar',
+        password: 'foo.bar',
+        passwordConfirmation: 'foo.bar',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
