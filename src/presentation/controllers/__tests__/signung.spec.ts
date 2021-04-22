e41ad3b8936f6feeb319c921880f2807f1a75e31
Guilherme Ventura de Souza <venturalp@gmail.com>
2021-04-21 21:09:00 -0300

    test(signup): test status code 400 expected when no name is provided

--- src/presentation/controllers/__tests__/signung.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('SignUp Controller', () => {
-  test('', () => {
-
-  })
-})
---
+++ src/presentation/controllers/__tests__/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from '../signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    // sut = System Under Test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'foo@bar.com',
+        password: 'foo.bar',
+        passwordConfirmation: 'foo.bar',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
